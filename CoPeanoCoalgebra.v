86baae9a14a16eac7b9d71ea072357a04f068000
pboutill <pboutill@0cf17b13-060f-0410-b1b1-c666bec9822a>
2012-07-16 17:23:02 +0000

    Get ready for the change of syntax in patterns
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/coq-contribs/trunk@2033 0cf17b13-060f-0410-b1b1-c666bec9822a

--- CoPeanoCoalgebra.v
+++ CoPeanoCoalgebra.v
@@ -6,6 +6,7 @@
 (*                  <http://www.gnu.org/licenses/lgpl-3.0.html>         *)
 (************************************************************************)
 
+Set Asymmetric Patterns.
 Require Import WeaklyFinalCoalgebra.
 
 (** Implementation of the weakly final coalgebra of potentially
